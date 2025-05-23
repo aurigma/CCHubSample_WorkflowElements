53be1e9167e36203fbd3659c1af92930d78e75c1
Andrey Kernichnyy <a.kernichniy@aurigma.com>
2025-05-23 13:28:57 +0700

    Added example for using getImagesDpiInfo method

---
+++ src/client/interfaces/template-editor.ts
@@ -0,0 +1,9 @@
+export interface IImageDpiInfo {
+    /**
+     * Design atoms model 
+     * https://customerscanvas.com/dev/editors/design-atoms-js/reference/design-atoms-model/item.html#_aurigma_design_atoms_model_Item_class
+     */
+    item: any;
+    dpi: number;
+    qualityState: "None" | "Good" | "Warning" | "Bad";
+  }
