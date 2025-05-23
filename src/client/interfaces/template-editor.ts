aae299904b310e22254f836fccf67b61cc5b37d4
Andrey Kernichnyy <a.kernichniy@aurigma.com>
2025-05-23 13:38:02 +0700

    Change comments

--- src/client/interfaces/template-editor.ts
+++ src/client/interfaces/template-editor.ts
@@ -1,6 +1,6 @@
 export interface IImageDpiInfo {
     /**
-     * Design atoms model 
+     * Design atoms model Item
      * https://customerscanvas.com/dev/editors/design-atoms-js/reference/design-atoms-model/item.html#_aurigma_design_atoms_model_Item_class
      */
     item: any;
