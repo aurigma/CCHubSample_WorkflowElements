3bd5296b15b5d984a551499720b196b0da16b910
Andrew Simontsev <andrew@aurigma.com>
2025-06-11 17:02:11 +0700

    Improved sample descriptions.

--- src/client/constants/samples.ts
+++ src/client/constants/samples.ts
@@ -1,27 +1,27 @@
 export const SAMPLES = [
     { 
         name: "Basic Simple Editor Sample", 
-        description: "Demonstrates how to embed Simple Editor to a page and save results as a project once you finish editing.", 
+        description: "Embed Simple Editor to a page, open a product in it, and save results as a project once you finish editing.", 
         path: "/SimpleEditorBasic" 
     },
     { 
         name: "Simple Editor with Plugin Api Sample", 
-        description: "Demonstrates how to use PluginApi with Simple Editor.", 
+        description: "When a product is loaded, use Simple Editor Plugin API to get an access to the design object model and modify it on the fly - convert the item container to Colorless for the Engraving printing technique.", 
         path: "/SimpleEditorWithPluginApi" 
     },
     { 
         name: "Basic Handy Editor Sample", 
-        description: "Demonstrates how to embed Handy Editor to a page and save results as a project once you finish editing.", 
+        description: "Embed Handy Editor to a page, open a product in it, and save results as a project once you finish editing.", 
         path: "/HandyEditorBasic" 
     },
     { 
         name: "Handy Editor with no PIM", 
-        description: "Demonstrates how to embed Handy Editor using just design and mockup ids (no PIM involved to configure the editor).", 
+        description: "Embed Handy Editor using just design and mockup ids (no PIM involved to configure the editor).", 
         path: "/HandyEditorNoPIM" 
     },
     { 
         name: "Template Editor", 
-        description: "An example how you can open Template Editor from your app to modify a public design template. Note, it is supposed that it is used in a custom \"admin\" app rather than in a customer-facing application.", 
+        description: "Open Template Editor from your app to modify a public design template. Note, it is supposed that it is used in a custom \"admin\" app rather than in a customer-facing application.", 
         path: "/TemplateEditor" 
     }
     
